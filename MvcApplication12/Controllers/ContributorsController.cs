cf69c0bb0447c07b962ebb496410a4cd33ed2ff9
Avrumi Friedman <info@lakewoodprogramming.com>
2017-06-29 22:33:36 -0400

    Initial commit

---
+++ MvcApplication12/Controllers/ContributorsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common.EntitySql;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication12.Models;
+using SimchaFund.Data;
+
+namespace MvcApplication12.Controllers
+{
+    public class ContributorsController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (TempData["Message"] != null)
+            {
+                ViewBag.Message = TempData["Message"];
+            }
+            var vm = new ContributorsIndexViewModel();
+            var mgr = new SimchaFundManager(Properties.Settings.Default.ConStr);
+            vm.Contributors = mgr.GetContributors();
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult New(Contributor contributor, decimal initialDeposit)
+        {
+            var mgr = new SimchaFundManager(Properties.Settings.Default.ConStr);
+            mgr.AddContributor(contributor);
+            var deposit = new Deposit
+            {
+                Amount = initialDeposit,
+                ContributorId = contributor.Id,
+                Date = contributor.Date
+            };
+            mgr.AddDeposit(deposit);
+            TempData["Message"] = "New Contributor Created! Id: " + contributor.Id;
+            return RedirectToAction("index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Contributor contributor)
+        {
+            var mgr = new SimchaFundManager(Properties.Settings.Default.ConStr);
+            mgr.UpdateContributor(contributor);
+            TempData["Message"] = "Contributor updated successfully";
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Deposit(Deposit deposit)
+        {
+            var mgr = new SimchaFundManager(Properties.Settings.Default.ConStr);
+            mgr.AddDeposit(deposit);
+            TempData["Message"] = "Deposit of $" + deposit.Amount + " recorded successfully";
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult History(int contribId)
+        {
+            var mgr = new SimchaFundManager(Properties.Settings.Default.ConStr);
+            IEnumerable<Deposit> deposits = mgr.GetDepositsById(contribId);
+            IEnumerable<Contribution> contributions = mgr.GetContributionsById(contribId);
+            #region list way without select
+            //List<Transaction> transactions2 = new List<Transaction>();
+            //foreach (Deposit deposit in deposits)
+            //{
+            //    transactions2.Add(new Transaction
+            //    {
+            //        Action = "Deposit",
+            //        Amount = deposit.Amount,
+            //        Date = deposit.Date
+            //    });
+            //}
+
+            //foreach (Contribution c in contributions)
+            //{
+            //    transactions2.Add(new Transaction
+            //    {
+            //        Action = "Contribution for the " + c.SimchaName + " simcha",
+            //        Amount = -c.Amount,
+            //        Date = c.Date
+            //    });
+            //}
+
+            //transactions2.OrderByDescending(t => t.Date);
+            #endregion
+            IEnumerable<Transaction> transactions = deposits.Select(d => new Transaction
+            {
+                Action = "Deposit",
+                Amount = d.Amount,
+                Date = d.Date
+            }).Concat(contributions.Select(c => new Transaction
+            {
+                Action = "Contribution for the " + c.SimchaName + " simcha",
+                Amount = -c.Amount,
+                Date = c.Date
+            })).OrderByDescending(t => t.Date);
+
+            var vm = new HistoryViewModel();
+            vm.Transactions = transactions;
+            Contributor contributor = mgr.GetContributors().First(c => c.Id == contribId);
+            vm.ContributorBalance = contributor.Balance;
+            vm.ContributorName = contributor.FirstName + " " + contributor.LastName;
+
+            return View(vm);
+        }
+    }
+}
---
+++ MvcApplication12/Scripts/site-scripts/contributor-index.js
@@ -0,0 +1,61 @@
+﻿$(function () {
+
+    $(function () {
+        $("#datepicker").datetimepicker({ format: 'MM/DD/YYYY' });
+        $("#contributor_created_at").datetimepicker({ format: 'MM/DD/YYYY' });
+    });
+
+    $("#new-contributor").on('click', function () {
+        $(".new-contrib").modal();
+    });
+
+    $(".deposit-button").on('click', function () {
+        var contribId = $(this).data('contribid');
+        $('[name="contributorId"]').val(contribId);
+
+        var tr = $(this).parent().parent();
+        var name = tr.find('td:eq(1)').text();
+        $("#deposit-name").text(name);
+
+        $(".deposit").modal();
+    });
+
+    $("#search").on('keyup', function () {
+        var text = $(this).val();
+        $("table tr:gt(0)").each(function () {
+            var tr = $(this);
+            var name = tr.find('td:eq(1)').text();
+            if (name.toLowerCase().indexOf(text.toLowerCase()) !== -1) {
+                tr.show();
+            } else {
+                tr.hide();
+            }
+        });
+    });
+
+    $("#clear").on('click', function () {
+        $("#search").val('');
+        $("tr").show();
+    });
+
+    $(".edit-contrib").on('click', function () {
+        var id = $(this).data('id');
+        var firstName = $(this).data('firstName');
+        var lastName = $(this).data('lastName');
+        var cell = $(this).data('cell');
+        var alwaysInclude = $(this).data('alwaysInclude');
+        var date = $(this).data('date');
+
+        var form = $(".new-contrib form");
+
+        $("#initialDepositDiv").hide();
+        form.append("<input type='hidden' name='id' value='" + id + "' />");
+        $("#contributor_first_name").val(firstName);
+        $("#contributor_last_name").val(lastName);
+        $("#contributor_cell_number").val(cell);
+        $("#contributor_always_include").prop('checked', alwaysInclude === "True");
+        $("#contributor_created_at").val(date);
+        $(".new-contrib").modal();
+        form.attr('action', '/contributors/edit');
+    });
+});
